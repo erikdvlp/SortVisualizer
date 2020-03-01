b64b3168eff596d7c5ec28ff0764c2b2e1935841
Erik Taylor <erikdvlp@gmail.com>
2020-02-29 21:46:44 -0500

    Window centered

--- src/app/Draw.java
+++ src/app/Draw.java
@@ -29,6 +29,7 @@ public class Draw extends JPanel
         //frame.setLayout(new FlowLayout());
         frame.getContentPane().setBackground(Color.black);
         frame.pack();
+        centerWindow(frame);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
@@ -55,6 +56,14 @@ public class Draw extends JPanel
     {
         bars = b;
         frame.getContentPane().repaint();
+    }
+
+    public static void centerWindow(Window frame)
+    {
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
+        frame.setLocation(x, y);
     }
 
     @Override
